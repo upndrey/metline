d6e595ec7ea8e5cbfec67f798c6256a32afe0fc1
upndrey <upndrey@yandex.ru>
2020-12-04 21:42:13 +0700

    Вертикальный скроллинг, горизонтальный скроллинг, меню, якоря и вторая страница с услугами

--- js/index.js
+++ js/index.js
@@ -1,4 +1,3 @@
-
 ymaps.ready(init);
 function init(){
     var myMap = new ymaps.Map("map",{center: [45.032878,39.022996],zoom: 15});
@@ -6,3 +5,40 @@ function init(){
     var myPlacemark = new ymaps.Placemark([45.032878,39.022996]);
     myMap.geoObjects.add(myPlacemark);
 }
+
+var myFullpage = new fullpage('#fullpage', {
+    anchors: ['firstScreen', 'catalog', 'services','callback'],
+    menu: '#menu',
+    navigation:false,
+    scrollOverflow: true,
+    scrollOverflowOptions: {
+        disablePointer: true
+    }
+});
+
+$("#send-message").click( function(e) {
+    e.preventDefault();
+    var name = $("#client-name").val();
+    var email = $("#client-email").val();
+    var phone = $("#client-phone").val();
+    var quest = $("#subject-list option:selected").text();
+    var message = $("#message").val();
+    var spam = $("#spam").val();
+    if ( spam.length == 0 ) {
+        $.ajax({
+            url: 'sender.php',
+            data: {
+                "name": name,
+                "email": email,
+                "phone": phone,
+                "quest": quest,
+                "message": message,
+
+            },
+            type: 'POST',
+            success: function(response){
+                alert(response);
+            }
+        });
+    }
+});
